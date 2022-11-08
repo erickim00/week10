3a3d62697a90e5c7b87f4c342319ba68b84c723d
Eric <skim588@my.bcit.ca>
2022-11-08 11:09:10 -0800

    Further practice in javascript - obj activity

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -29,3 +29,51 @@ fruits.unshift ("Mango");
 
 fruits.shift()
 
+for (let i = 0; i<fruits.length; i++)
+{
+    console.log(fruits[i])
+}
+
+for (let item of fruits)
+{
+    console.log(item);
+}
+
+const person = {
+    name: ['Bob', 'Smith'],
+    age: 32,
+    gender: 'male',
+    interests: ['music', 'skiing'],
+}
+
+console.log(`My name is ${person.name[0]} ${person.name[1]}. I am interested in ${person.interests[0]} and ${person.interests[1]}.`)
+
+for (let prop in person)
+{
+    console.log(`My ${prop} is ${person[prop]}`)
+}
+
+let student1 = {
+    ID: "A00022",
+    GPA: 3.0,
+    program: "CIT"
+};
+
+let student2 = {
+    ID: "A01000",
+    GPA: 3.1,
+    program: "CST"
+};
+
+let student3 = {
+    ID: "A00114",
+    GPA: 3.2,
+    program: "CIT"
+};
+let students = [student1, student2, student3];
+
+for(let stud of students)
+{
+    if (stud.program === "CIT" && stud["GPA"] > 3)
+    console.log(stud)
+}
