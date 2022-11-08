39021f759a6c2e4d1c296c4c99ae0fda998903a7
Eric <skim588@my.bcit.ca>
2022-11-08 09:54:28 -0800

    made for loop and while loop

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+alert("WARNING: ICBM MISSILES INCOMING")
+console.log("RUN")
+
+for (let i = 1; i <= 10; i++) {
+    console.log(2**i)
+}
+
+let i =1;
+while (i<=10) {
+    console.log(2**i);
+    i++;
+}
