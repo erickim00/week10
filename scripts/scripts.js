fd7079fc349bd6c6e2fff13c64d8f875d9738b65
Eric <skim588@my.bcit.ca>
2022-11-08 10:47:32 -0800

    Practiced javascript functions

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,3 +10,22 @@ while (i<=10) {
     console.log(2**i);
     i++;
 }
+
+let input = "";
+do{
+    input = prompt("What is your name?");
+} while (!isNaN(Number(input)));
+// console.log(input);
+
+// alert("Welcome " + input)
+alert(`Welcome ${input}`);
+
+let fruits = ["Apple", "Banana"];
+fruits.push("Kiwi");
+
+fruits.pop();
+
+fruits.unshift ("Mango");
+
+fruits.shift()
+
